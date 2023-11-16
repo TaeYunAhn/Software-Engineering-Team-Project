ba6bc712c83048361e8b7c8494e78a301e6c5ed2
Jihoo <jikim08@naver.com>
2023-11-17 00:05:38 +0900

    "feat: Add easter egg"
    -이스터에그 함수 만들었습니다
    -문자와 도트아트 출력

--- Calculator/operation/operation.py
+++ Calculator/operation/operation.py
@@ -2,9 +2,6 @@ def operation(data):
     nums = []
     operator = []
 
-    #에러 처리 코드 넣기(data에 값이 정상적으로 들어왔는지, 연산자가 여러개 들어오진 않았는지)
-    #isError(data)
-
     for i in range(0 , len(data)):
         if i % 2 == 0:
             nums.append(int(data[i]))
