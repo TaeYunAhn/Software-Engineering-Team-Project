adf7c8a3ab4f49785ffea99e0824ca41cfe55443
Jihoo <jikim08@naver.com>
2023-11-16 12:20:12 +0900

    fix: modify to delete difference operation [#4]

--- Calculator/operation/operation.py
+++ Calculator/operation/operation.py
@@ -2,7 +2,7 @@ def operation(data):
     nums = []
     operator = []
 
-    #에러 처리 코드 넣기(data에 값이 정상적으로 들어왔는지)
+    #에러 처리 코드 넣기(data에 값이 정상적으로 들어왔는지, 연산자가 여러개 들어오진 않았는지)
     #isError(data)
 
     for i in range(0 , len(data)):
@@ -11,25 +11,13 @@ def operation(data):
         else:
             operator.append(data[i])
 
-
-    #곱셈을 먼저 모두 처리하고 덧셈만 남김
-
     answer = nums[0]
-    i = 0
-    while i < len(operator):
-        if i >= len(operator):
-            break
-        if operator[i] == "*":
-            nums[i] = nums[i]*nums[i+1]
-            del nums[i+1]
-            del operator[i]
-            i -= 1
-        i += 1
-
-    #나머지 덧셈뺄셈 처리
-    for i in range(0,len(operator)):
+    for i in range(0, len(operator)):
         if operator[i] == "+":
             answer += nums[i+1]
         elif operator[i] == "-":
-            answer -= nums[i+1]
+            answer -= nums[i + 1]
+        elif operator[i] == "*":
+            answer *= nums[i + 1]
+
     return answer
